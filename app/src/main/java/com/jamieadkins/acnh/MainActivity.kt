4696d61b025165b8bcdea512fed18c15d2a7e3de
Jamie Adkins <jamie.adkins@pulselive.com>
2020-03-28 16:02:41 +0000

    Added bottom nav and setup with nav component

--- app/src/main/java/com/jamieadkins/acnh/MainActivity.kt
+++ app/src/main/java/com/jamieadkins/acnh/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.jamieadkins.acnh
 
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.jamieadkins.acnh.PredictorApplication.Companion.coreComponent
+import com.jamieadkins.acnh.databinding.ActivityMainBinding
 import com.jamieadkins.acnh.di.DaggerAndroidActivity
 import com.jamieadkins.acnh.di.DaggerAppComponent
 
 class MainActivity : DaggerAndroidActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onInject() {
         DaggerAppComponent.builder()
@@ -17,6 +22,10 @@ class MainActivity : DaggerAndroidActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
